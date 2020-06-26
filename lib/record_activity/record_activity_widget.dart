343d90d9e9b47215989b3746541c6111fced11b3
Adam Pociejowski <valverde12345@gmail.com>
2020-06-26 14:34:59 +0200

    Implemented location service

--- lib/record_activity/record_activity_widget.dart
+++ lib/record_activity/record_activity_widget.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/location/location_observer.dart';
+import 'package:flutterapp/location/location_service.dart';
+import 'package:location/location.dart';
 
 class RecordActivityWidget extends StatefulWidget {
   @override
   _RecordActivityWidgetState createState() => _RecordActivityWidgetState();
 }
 
-class _RecordActivityWidgetState extends State<RecordActivityWidget> {
+class _RecordActivityWidgetState extends State<RecordActivityWidget> implements LocationObserver {
+  var locationService = new LocationService();
+  String _locationField = 'Searching for location';
+
   @override
   Widget build(BuildContext context) {
+    locationService.registerObserver(this);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('Record activity'),
       ),
-      body: Text('Test text'),
+      body: Center(
+        child: Text(_locationField),
+      ),
     );
+  }
+
+  @override
+  void onLocationChanged(LocationData locationData) {
+    print('Location [ lat: ${locationData.latitude}, lng: ${locationData.longitude} ]');
+    setState(() {
+      _locationField = 'Location [ lat: ${locationData.latitude}, lng: ${locationData.longitude} ]';
+    });
   }
 }
